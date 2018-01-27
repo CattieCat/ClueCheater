de480d085f790faf055e60dba35f59fdc617e208
Jinjing Ma <majinjing1990@gmail.com>
2018-01-26 23:04:39 -0800

    response activities

---
+++ app/src/main/java/project/jinjingm/cluecheater/RespondToMySuggestionActivity.java
@@ -0,0 +1,13 @@
+package project.jinjingm.cluecheater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RespondToMySuggestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_respond_to_my_suggestion);
+    }
+}
---
+++ app/src/main/java/project/jinjingm/cluecheater/RespondToSuggestionActivity.java
@@ -0,0 +1,13 @@
+package project.jinjingm.cluecheater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RespondToSuggestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_respond_to_suggestion);
+    }
+}
